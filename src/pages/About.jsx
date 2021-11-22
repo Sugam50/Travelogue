ff204821229cda9e8637db6eace38f2feff284ed
Sugam Agrawal <71265266+Sugam50@users.noreply.github.com>
2021-11-23 00:20:22 +0530

    about added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,23 +10,22 @@ function About() {
         <Card className="Card" style={{ width: "18rem" }}>
           <Card.Img
             variant="top"
-            src="https://avatars.githubusercontent.com/u/77954495?s=400&u=3110e72130bc8a7faee7b2aa39ec2664ff2e748d&v=4"
-          />
+            src="https://media-exp1.licdn.com/dms/image/C5603AQHAyz3EfDiwqA/profile-displayphoto-shrink_400_400/0/1637592318604?e=1643241600&v=beta&t=5V4-Nx9gInsxH-GRVwWdN_td_cLvpBYpAmeLcU5wOxs"   />
           <Card.Body>
             <Card.Title>Pradyumn Rajak</Card.Title>
             <Card.Text>Frontend Developer | Ml and Al enthusiast</Card.Text>
             <ButtonGroup>
-              <a href="https://www.linkedin.com/in/rajulkoshta/">
+              <a href="https://www.linkedin.com/in/pradyumna-rajak-7a1077204/">
                 <Button variant="light">
                   <Linkedin />
                 </Button>
               </a>
-              <a href="https://github.com/rajulkoshta">
+              <a href="https://github.com/kunalcode12">
                 <Button variant="light">
                   <GitHub />
                 </Button>
               </a>
-              <a href="https://twitter.com/RajulKoshta">
+              <a href="https://twitter.com/">
                 <Button variant="light">
                   <Twitter />
                 </Button>
@@ -66,23 +65,23 @@ function About() {
         <Card className="Card" style={{ width: "18rem" }}>
           <Card.Img
             variant="top"
-            src="https://avatars.githubusercontent.com/u/89150017?v=4/100px180"
+            src="https://cdn.discordapp.com/attachments/912379186419753012/912413520346361876/imageonline-co-roundcorner_5.png"
           />
           <Card.Body>
             <Card.Title>Sourabh Soni</Card.Title>
             <Card.Text>Frontend Developer | techie | Code lover</Card.Text>
             <ButtonGroup>
-              <a href="https://linkedin.com/in/tabhay563">
+              <a href="https://linkedin.com/">
                 <Button variant="light">
                   <Linkedin />
                 </Button>
               </a>
-              <a href="https://github.com/tabhay563">
+              <a href="https://github.com/">
                 <Button variant="light">
                   <GitHub />
                 </Button>
               </a>
-              <a href="https://twitter.com/tabhay56">
+              <a href="https://twitter.com/">
                 <Button variant="light">
                   <Twitter />
                 </Button>
@@ -93,23 +92,23 @@ function About() {
         <Card className="Card" style={{ width: "18rem" }}>
           <Card.Img
             variant="top"
-            src="https://avatars.githubusercontent.com/u/89150017?v=4/100px180"
+            src="https://cdn.discordapp.com/attachments/912379186419753012/912402312704708748/imageonline-co-roundcorner.png"
           />
           <Card.Body>
             <Card.Title>Vishal Dangiwala</Card.Title>
             <Card.Text>Frontend Developer | techie | Code lover</Card.Text>
             <ButtonGroup>
-              <a href="https://linkedin.com/in/tabhay563">
+              <a href="https://linkedin.com/vishal-dangiwala-60b5361a5/">
                 <Button variant="light">
                   <Linkedin />
                 </Button>
               </a>
-              <a href="https://github.com/tabhay563">
+              <a href="https://github.com/">
                 <Button variant="light">
                   <GitHub />
                 </Button>
               </a>
-              <a href="https://twitter.com/tabhay56">
+              <a href="https://twitter.com/">
                 <Button variant="light">
                   <Twitter />
                 </Button>
